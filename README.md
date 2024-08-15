d5e4d45e4a8df513cd64953827cc8116e4ef1da2
kokilaw <kokilapc@gmail.com>
2024-08-16 00:07:07 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# menu-service
+
+## Requirements
+
+For building and running the application you need:
+- Java 17
+- Quarkus 3.13.2
+
+
+
---
+++ src/test/java/com/au/eatclub/LambdaHandlerIT.java
@@ -0,0 +1,7 @@
+package com.au.eatclub;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class LambdaHandlerIT extends LambdaHandlerTest {
+}
