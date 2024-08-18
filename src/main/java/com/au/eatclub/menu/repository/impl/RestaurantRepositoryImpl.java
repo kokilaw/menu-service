f5077263c7dfd998796de9c9d6146ec12bfd77dd
kokilaw <kokilapc@gmail.com>
2024-08-18 14:08:44 +1000

    task(): checkstyle fixes

--- src/main/java/com/au/eatclub/menu/repository/impl/RestaurantRepositoryImpl.java
+++ src/main/java/com/au/eatclub/menu/repository/impl/RestaurantRepositoryImpl.java
@@ -1,10 +1,7 @@
 package com.au.eatclub.menu.repository.impl;
 
 import com.au.eatclub.menu.repository.RestaurantRepository;
-import com.au.eatclub.menu.repository.model.RestaurantEntity;
 import jakarta.enterprise.context.ApplicationScoped;
-
-import java.util.Optional;
 
 @ApplicationScoped
 public class RestaurantRepositoryImpl implements RestaurantRepository {
